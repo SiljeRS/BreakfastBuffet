67d5a14c0f0bd459bda768c8e278b4ed5cd8a083
SiljeRS <68823039+SiljeRS@users.noreply.github.com>
2022-04-19 10:20:33 +0200

    Breakfast class added

--- BreakfastBuffet/Pages/Reception.cshtml.cs
+++ BreakfastBuffet/Pages/Reception.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BreakfastBuffet.Data;
 
 namespace BreakfastBuffet.Pages
 {
     public class ReceptionModel : PageModel
     {
+        private readonly MyDbContext _context;
+        public ReceptionModel(MyDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
+        }
+
+        [BindProperty]
+        public InputModel? Input { get; set; } = new InputModel();
+
+        public class InputModel
+        {
+            [Required]
+            public string Name { get; set; }
         }
     }
 }
