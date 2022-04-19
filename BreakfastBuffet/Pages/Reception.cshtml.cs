8a0c225f2b3cad21c99bc243102b5016d6de3ccf
Sandra Krogh <Sandrask85@hotmail.com>
2022-04-19 09:44:59 +0200

    Html pages added

---
+++ BreakfastBuffet/Pages/Reception.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BreakfastBuffet.Pages
+{
+    public class ReceptionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
